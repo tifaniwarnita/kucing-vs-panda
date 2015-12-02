53f8b86c06eb45593ac4a85893f290a4b5166ec5
Tifani Warnita <tiffayumuyuka@gmail.com>
2015-12-02 19:22:42 +0700

    Add packet project

---
+++ Kucing vs Panda Packet/src/kucing/vs/panda/packet/Identifier.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kucing.vs.panda.packet;
+
+/**
+ *
+ * @author Tifani
+ */
+public class Identifier {
+    public static final int LOGIN = 1;
+    public static final int CHAT = 2;
+}
---
+++ Kucing vs Panda Packet/src/kucing/vs/panda/packet/MessagePacket.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kucing.vs.panda.packet;
+
+/**
+ *
+ * @author Tifani
+ */
+public class MessagePacket implements Packet {
+    private static final int identifier = Identifier.CHAT;
+    private String message;
+    
+     public MessagePacket() {
+        message = "";
+    }
+    
+    public MessagePacket(String message) {
+        this.message = message;
+    }
+    
+    @Override
+    public int getIdentifier() {
+        return identifier;
+    }
+
+    @Override
+    public Object getMessage() {
+        return message;
+    }
+    
+}
